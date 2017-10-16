09632da1b79a6c242276e17f6d36284862cef853
hlccare <280108904@qq.com>
2017-10-17 00:20:36 +0800

    Chapter9 Done 无邮件确认

--- blog/__init__.py
+++ blog/__init__.py
@@ -4,11 +4,16 @@ from flask import Flask
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 from flask_mail import Mail
+from flask_login import LoginManager
 from config import config
 
 bootstrap = Bootstrap()
 db = SQLAlchemy()
 mail = Mail()
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
+
 
 
 def create_app(config_name):
@@ -19,8 +24,15 @@ def create_app(config_name):
     bootstrap.init_app(blog)
     mail.init_app(blog)
     db.init_app(blog)
+    login_manager.init_app(blog)
+
+    with blog.app_context():
+        db.create_all()
 
     from .main import main as main_blueprint
     blog.register_blueprint(main_blueprint)
+
+    from .auth import auth as auth_blueprint
+    blog.register_blueprint(auth_blueprint, url_prefix='/auth')
 
     return blog
---
+++ blog/auth/views.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from flask import render_template, redirect, url_for, flash, request
+from flask_login import login_user, login_required, logout_user
+from . import auth
+from ..models import User
+from .forms import LoginForm, RegisterForm
+from .. import db
+
+
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    login_Form = LoginForm()
+    if login_Form.validate_on_submit():
+        user = User.query.filter_by(email=login_Form.email.data).first()
+        if user is not None and user.verify_password(login_Form.password.data):
+            login_user(user, login_Form.remember_me.data)
+            return redirect(url_for('main.index'))
+        else:
+            flash('帐号或者密码错误！请重新输入')
+    return render_template('auth/login.html', form=login_Form)
+
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    flash('已成功退出！')
+    return redirect(url_for('main.index'))
+
+
+@auth.route('/register', methods=['GET', 'POST'])
+def register():
+    register_Form = RegisterForm()
+    if register_Form.validate_on_submit():
+        user = User(email=register_Form.email.data,
+                    username=register_Form.username.data,
+                    password=register_Form.password.data)
+        db.session.add(user)
+        flash('已成功注册')
+        return redirect(url_for('auth.login'))
+    return render_template('auth/register.html', form=register_Form)
